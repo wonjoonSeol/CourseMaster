f4ac6bc4b0051d9e7ac8e4adbe5b4ef5763b6c10
Wonjoon Seol <wonjoon.seol@kcl.ac.uk>
2017-10-26 01:44:30 +0100

    update

--- docs/scripts/javascripts.js
+++ docs/scripts/javascripts.js
@@ -1,8 +1,63 @@
-function switchOn(id) {
+var term1 = 0;
+var term2 = 0;
+
+function switchOn(id, term) {
+	alert("sex");
     var x = document.getElementById(id);
-    x.style.display = "inline-block";
+	if (x.style.display == 'none') {
+		x.style.display = 'inline-block';
+		if (term == 1) {
+			term1 += 1;
+			issueWarning();
+		} else if (term == 2) {
+			term2 += 1;
+			issueWarning();
+		}
+	}
 }
-function switchOff(id) {
+function switchOff(id, term) {
     var x = document.getElementById(id);
-    x.style.display = "none";
+
+	if (x.style.display == 'inline-block') {
+		x.style.display = 'none';
+
+		if (term == 1 && term1 > 0) {
+			term1--;
+			issueWarning();
+		}else if (term2 == 2 && term2>0) {
+			term2--;
+			issueWarning();
+		}
+	}
+}
+
+function issueWarning() {
+	var y;
+	y = document.getElementById(term1Alert);
+	if (term1 < 3) {
+		y.style.display = "block";
+	} else {
+		y.style.display = "none";
+	}
+
+	y = document.getElementById(term2Alert);
+	if (term2 < 3) {
+		y.style.display = "block";
+	} else {
+		y.style.display = "none";
+	}
+
+	y = document.getElementById(8modules);
+	if (term1 + term2 < 8) {
+		y.style.display = "block";
+	} else {
+		y.style.display = "none";
+	}
+
+	y = document.getElementById(moduleWarning);
+	if (term1 + term2 == 8) {
+		y.style.display = "block";
+	} else {
+		y.style.display = "none";
+	}
 }
